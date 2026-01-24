a6bb335195233106e1638b68c600c19d05985bb4
Nono Geulin <veryblueviolin@gmail.com>
2026-01-24 01:29:51 +0100

    Create Project with API CALLS

---
+++ CallAPI/RandoStat/APIToRandoStat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace NoNiDev.CallAPI.RandoStat
+{
+    public static class APIToRandoStat
+    {
+        private static string _baseURL = "";
+        public static void InitURL (string baseURL)
+        {
+            _baseURL = baseURL;
+        }
+        public static async Task<string> AddArchipel(RandoStatData randoStat)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            
+
+
+            string jsonString = JsonSerializer.Serialize<RandoStatData>(randoStat, options);
+            
+            using var client = new HttpClient();
+            using var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(_baseURL, content);
+            var responseText = await response.Content.ReadAsStringAsync();
+            return responseText;
+        }
+
+        
+    }
+}
---
+++ CallAPI/RandoStat/RandoStatData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using NoNiDev.SpoilerArchipelagoParser.RandoStats;
+
+namespace NoNiDev.CallAPI.RandoStat
+{
+    public enum RandoStatAction
+    {
+       addJoueur,
+       addArchipel,
+       ping
+    }
+
+    public class RandoStatData
+    {
+        [JsonIgnore]
+        public RandoStatAction Action { get; set; }
+        [JsonPropertyName("action")]
+        public string ActionStr => Action.ToString();
+        public PayloadAddArchipel Payload { get; set; }
+    }
+    public class PayloadAddArchipel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        [JsonPropertyName("games")]
+        public List<ArchippelagoSlot> Slots { get; set; }
+    }
+}
