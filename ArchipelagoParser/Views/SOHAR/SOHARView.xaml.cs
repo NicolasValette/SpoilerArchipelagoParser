84f423b6a98dfc3775d5895364512844a097a219
NikoMyoji <nicolas.vlte@gmail.com>
2026-02-02 10:54:39 +0100

    Move & clean Main views

--- ArchipelagoParser/Views/SOHAR/SOHARView.xaml.cs
+++ ArchipelagoParser/Views/SOHAR/SOHARView.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NoNiDev.ArchipelagoParser.Views
+namespace NoNiDev.ArchipelagoParser.Views.SOHAR
 {
     /// <summary>
     /// Interaction logic for SOHARView.xaml
